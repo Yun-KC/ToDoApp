7365d8440ac8ebeaa73051672128b2ecb384a7ce
YUN-KC <wjdrlfgg@gmail.com>
2022-07-08 15:21:10 +0900

    드래그앤드랍 구현

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,4 +1,3 @@
-import type { Identifier, XYCoord } from 'dnd-core';
 import type { FC } from 'react';
 import React, { useRef } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
@@ -28,58 +27,63 @@ interface DragItem {
 
 export const Card: FC<CardProps> = ({ id, text, index, moveCard }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [{ handlerId }, drop] = useDrop<DragItem, void, { handlerId: Identifier | null }>({
+  const [, drop] = useDrop({
     accept: ItemTypes.CARD,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      };
-    },
     hover(item: DragItem, monitor) {
       if (!ref.current) {
         return;
       }
+
+      // 드래그 중인 요소의 인덱스
       const dragIndex = item.index;
+
+      // 드랍될 요소의 인덱스
       const hoverIndex = index;
 
-      // Don't replace items with themselves
+      // 같은 요소끼리는 바뀌지 않도록
       if (dragIndex === hoverIndex) {
         return;
       }
 
-      // Determine rectangle on screen
+      // https://developer.mozilla.org/ko/docs/Web/API/Element/getBoundingClientRect
+      // 엘리먼트의 크기와 뷰포트에 상대적인 위치 정보를 제공하는 DOMRect 객체를 반환합니다.
       const hoverBoundingRect = ref.current?.getBoundingClientRect();
 
-      // Get vertical middle
+      // 엘리먼트의 상대적인 y좌표 위치의 반
       const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
 
-      // Determine mouse position
+      // 마우스 포인터의 x, y 좌표
       const clientOffset = monitor.getClientOffset();
 
-      // Get pixels to the top
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+      // 드랍할 요소의 top 값과 마우스 포인터의 y 값 빼기
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
 
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
+      // 마우스가 항목 높이의 절반을 넘은 경우에만 이동
+      // 아래로 끌 때는 커서가 요소의 50% 미만일 때만 이동합니다.
+      // 위쪽으로 끌 때는 커서가 요소의 50% 이상일 때만 이동합니다.
 
-      // Dragging downwards
+      // 드래그 중인 요소의 인덱스: dragIndex
+      // 드랍될 요소의 인덱스: hoverIndex
+      // 드랍될 요소에서 마우스 포인트의 상대적인 위치 : hoverClientY
+      // 드랍될 요소의 상대적인 중앙 Y 값: hoverMiddleY
+
+      // dragIndex가 hoverIndex보다 작은 경우와
+      // hoverClientY가 hoverMiddleY보다 작은 경우
+      // 무시함.
       if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
         return;
       }
 
-      // Dragging upwards
+      // dragIndex가 hoverIndex보다 큰 경우와
+      // hoverClientY가 hoverMiddleY보다 큰 경우
+      // 무시함.
       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
         return;
       }
 
-      // Time to actually perform the action
+      // 나머지 경우에 카드를 바꿔줌
       moveCard(dragIndex, hoverIndex);
-
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
+      // 카드를 바꿔줬다면 드래그 중인 요소의 index를 호버 인덱스와 바꿔줌
       item.index = hoverIndex;
     },
   });
@@ -87,7 +91,7 @@ export const Card: FC<CardProps> = ({ id, text, index, moveCard }) => {
   const [{ isDragging }, drag] = useDrag({
     type: ItemTypes.CARD,
     item: () => {
-      return { id, index };
+      return { id, index, text };
     },
     collect: (monitor: any) => ({
       isDragging: monitor.isDragging(),
@@ -97,7 +101,7 @@ export const Card: FC<CardProps> = ({ id, text, index, moveCard }) => {
   const opacity = isDragging ? 0 : 1;
   drag(drop(ref));
   return (
-    <div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
+    <div ref={ref} style={{ ...style, opacity }}>
       {text}
     </div>
   );
