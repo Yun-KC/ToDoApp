ce4b87fb38ed556ba17152105e258625d112416e
Yun-KC <wjdrlfgg@gmail.com>
2022-07-12 19:21:46 +0900

    modify app component size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { Container } from './components/Container';
-import GlobalStyle from './GlobalStyle';
-import styled from 'styled-components';
+import React from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Container } from "./components/Container";
+import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
 
 const AppContainer = styled.div`
   display: grid;
-  width: 1000px;
-  height: 900px;
+  width: 100vw;
+  height: 100vh;
   grid-template-rows: 1fr 3fr 1fr;
   grid-template-columns: 1fr 1fr;
   grid-template-areas:
-    'header header '
-    'main cardlist'
-    'footer footer';
+    "header header "
+    "main cardlist"
+    "footer footer";
 `;
 
 const App = () => {
