eb2f6d46825d2f0869d30b5a73eb1ca07168d215
YUN-KC <wjdrlfgg@gmail.com>
2022-07-15 17:32:42 +0900

    작은 따옴표 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { Container } from "./components/Container";
-import GlobalStyle from "./GlobalStyle";
-import styled from "styled-components";
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Container } from './components/Container';
+import GlobalStyle from './GlobalStyle';
+import styled from 'styled-components';
 
 const AppContainer = styled.div`
   display: grid;
@@ -12,9 +12,9 @@ const AppContainer = styled.div`
   grid-template-rows: 1fr 3fr 1fr;
   grid-template-columns: 1fr 1fr;
   grid-template-areas:
-    "header header "
-    "main cardlist"
-    "footer footer";
+    'header header '
+    'main cardlist'
+    'footer footer';
 `;
 
 const App = () => {
